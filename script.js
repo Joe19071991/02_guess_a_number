61d3193c516264173d28b42b3d3005a441ecaf55
Joe19071991 <lauranove66@gmail.com>
2024-12-08 12:06:13 +0100

    première étape validé mon prompt avec un chiffre valide, chiffre à virgule accepté

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
 function getNumber() {
-  let givenNumber = prompt("Veuillez entrer un nombre");
-  givenNumber = parseFloat(givenNumber); //parseFloat = chiffre à virgule accepté, parseInt = chiffre rond
+  const givenNumber = parseFloat(prompt("Veuillez saisir un chiffre"));
   if (isNaN(givenNumber)) {
-    alert("Le nombre n'est pas valide");
-  } else {
-    alert("le nombre que vous avez entré est : " + givenNumber);
+    return null;
   }
   return givenNumber;
 }
-getNumber();
 
-function didIWin(givenNumber) {
-  const number = 22;
-
-  if (givenNumber < number) {
-    alert("plus grand");
-  } else if (givenNumber > number) {
-    alert("plus petit");
-  } else if (givenNumber === number) {
-    alert("bravo ! Vous avez deviné le chiffre");
-  } else {
-    alert("entrée invalide");
-  }
-}
-
-let chiffre = parseFloat(prompt("saisissez le chiffre à deviner : "));
-didIWin(chiffre);
+console.log("Le chiffre choisi est un chiffre valide ? " + getNumber());

be6ce82723bac2c1c928108c1fffb4f198f515ba
Joe19071991 <lauranove66@gmail.com>
2024-12-08 12:20:26 +0100

    partie 2 de l'exercice, Ajout de la fonction didIWin pour deviner un chiffre avec des alertes adaptées + test de la fonction.

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ function getNumber() {
   }
   return givenNumber;
 }
+//console.log("Le chiffre choisi est un chiffre valide ? " + getNumber());
 
-console.log("Le chiffre choisi est un chiffre valide ? " + getNumber());
+function didIWin(givenNumber) {
+  const targetNumber = 22;
+  if (givenNumber < targetNumber) {
+    alert("Le chiffre à deviner est plus grand.");
+    return false;
+  } else if (givenNumber > targetNumber) {
+    alert("Le chiffre à deviner est plus petit.");
+    return false;
+  }
+  alert("Bravo ! Vous avez deviné le chiffre !");
+  return true;
+}
+
+//console.log("Est-ce le bon chiffre à deviner ? => " + didIWin(67));
+//console.log("Est-ce le bon chiffre à deviner ? => " + didIWin(8));
+//console.log("Est-ce le bon chiffre à deviner ? => " + didIWin(22));
